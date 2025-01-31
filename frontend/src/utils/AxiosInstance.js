2ced6612c7e0b8e2d0192031e088a3fcff658697
Kishore2268 <kishore@anivarti.in>
2025-01-31 16:55:50 +0530

    first commit

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://anivarti-admin.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
